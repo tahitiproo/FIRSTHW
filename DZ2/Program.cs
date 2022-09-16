e3a8876550fcd3484f6f5e7b2e26903b57e21fea
tahitiproo <113210311+tahitiproo@users.noreply.github.com>
2022-09-16 23:55:54 +0300

    Add files via upload

---
+++ DZ2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double exponenta = Math.Exp(1);
+            double exp = Math.Round(exponenta, 1);
+            Console.WriteLine(exp);
+            Console.ReadKey();
+
+        }
+    }
+}
